054b194cb8cd936e6ec324d6f9c7025101ce48b5
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-04-03 16:24:42 +0000

    add promise to add track from uri to Playlist.js

--- src/Containers/Playlist/Playlist.js
+++ src/Containers/Playlist/Playlist.js
@@ -5,29 +5,50 @@ import Track from '../../Components/Track/Track'
 export default function Playlist({ playList, removePlayListTrack, setName, name }) {
     const [nameChange, setNameChange] = useState(false);
     const retrieveID = async () => {
-    const res = await fetch('https://api.spotify.com/v1/me?access_token=' + window.localStorage.getItem("token"))
-    const data = await res.json();
-    const USER_ID = data.id;
-    return USER_ID;
+        const res = await fetch('https://api.spotify.com/v1/me?access_token=' + window.localStorage.getItem("token"))
+        const data = await res.json();
+        const USER_ID = data.id;
+        return USER_ID;
     }
 
     const POSTPLAYLIST = async () => {
         const userID = await retrieveID();
-        const url = `https://api.spotify.com/v1/users/${userID}/playlists`
+        const url = `https://api.spotify.com/v1/users/${userID}/playlists`;
         const res = await fetch(url, {
-  method: "POST",
-  body: JSON.stringify({
-    "name": "name",
-    "description": "New playlist description",
-    "public": false
-}),
-  headers: {
-    Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-    "Content-type": "application/json; charset=UTF-8"
-  }
-});
-    await res.json();
-    
+            method: "POST",
+            body: JSON.stringify({
+                "name": text,
+                "description": "New playlist description",
+                "public": false
+            }),
+            headers: {
+                Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        const data = await res.json();
+        const PLAYLIST_ID = await data.id;
+        return PLAYLIST_ID;
+    }
+
+    const ADDTRACKS = async () => {
+        const playListID = await POSTPLAYLIST();
+        const url = `https://api.spotify.com/v1/playlists/${playListID}/tracks`
+        let playListURIs = playList.map(track => track.uri);
+        console.log(playListURIs)
+        const res = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                    "uris": playListURIs,
+                    "position": 0,
+            }),
+            headers: {
+                Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+
+        console.log(res.json());
     }
     const [text, setText] = useState('')
     const handleClick = () => {
@@ -45,11 +66,11 @@ export default function Playlist({ playList, removePlayListTrack, setName, name 
     }
     return (
         <div className={styles.playList}>
-            {nameChange ? 
-            <form onSubmit={handleSubmit} className={styles.name}>
-                <input type="text" name="playlistName" placeholder={name} onChange={handleChange}/>
-                <button type="submit">Save</button>
-            </form>
+            {nameChange ?
+                <form onSubmit={handleSubmit} className={styles.name}>
+                    <input type="text" name="playlistName" placeholder={name} onChange={handleChange} />
+                    <button type="submit">Save</button>
+                </form>
                 :
                 <div className={styles.name}>
                     <h2>{name}</h2>
@@ -57,7 +78,7 @@ export default function Playlist({ playList, removePlayListTrack, setName, name 
                 </div>
             }
             {playList.map((track) => <Track track={track} key={`PlayList_${track.id}`} handleClick={removePlayListTrack} text={'x'} />)}
-            <button onClick={POSTPLAYLIST}>EXPORT</button>
+            <button onClick={ADDTRACKS}>EXPORT</button>
         </div>
     )
 }
