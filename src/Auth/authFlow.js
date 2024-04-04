de067b99358b37f830cbc5c727590cd73672e27a
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-04-04 14:34:39 +0000

    fix redirect url in authFlow

--- src/Auth/authFlow.js
+++ src/Auth/authFlow.js
@@ -1,7 +1,7 @@
 const authFlow = () => {
 
   const CLIENT_ID = 'e191ea5f28f7459eb45d6e9d24eb552f';
-  const REDIRECT_URI = "https://congenial-succotash-jxwv5qq4w9pfq7gw-3000.app.github.dev/";
+  const REDIRECT_URI = "https://didactic-meme-7g469ppr4952ww5r-3000.app.github.dev/";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
   const SCOPE = 'playlist-modify-public playlist-modify-private'
