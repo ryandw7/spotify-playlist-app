1d3c94e99cf567d00045d08416f1b48828f27d02
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-04-04 18:06:06 +0000

    Remove post playlist pipeline from Playlist.js and create API PIPELINE folder with post playlist module

---
+++ src/API Pipelines/postPlayList.js
@@ -0,0 +1,63 @@
+const retrieveID = async () => {
+        
+    try {
+        const res = await fetch('https://api.spotify.com/v1/me?access_token=' + window.sessionStorage.getItem("token"))
+        const data = await res.json();
+        const USER_ID = data.id;
+        return USER_ID;
+    } catch (err) {
+        console.log(`ERROR: ${err}, Unable to retrieve ID`);
+    }
+}
+
+const CREATEPLAYLIST = async (name) => {
+
+    const userID = await retrieveID();
+    const url = `https://api.spotify.com/v1/users/${userID}/playlists`;
+    try {
+        const res = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                "name": name,
+                "description": "New playlist description",
+                "public": false
+            }),
+            headers: {
+                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        const data = await res.json();
+        const PLAYLIST_ID = await data.id;
+        return PLAYLIST_ID;
+    } catch (err) {
+        console.log(`ERROR: ${err}, Unable to POST Playlist`);
+    }
+}
+
+
+export default async function postPlaylist(trackList, playListName){
+
+    const playListID = await CREATEPLAYLIST(playListName);
+    const url = `https://api.spotify.com/v1/playlists/${playListID}/tracks`;
+    
+    let playListURIs = trackList.map(track => track.uri);
+
+    try {
+        console.log(playListURIs)
+        const res = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                "uris": playListURIs,
+                "position": 0,
+            }),
+            headers: {
+                Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        console.log(res.json());
+    } catch (err) {
+        console.log(`ERROR: ${err}: Unable to POST tracks to playlist`);
+    }
+}
--- src/Containers/Playlist/Playlist.js
+++ src/Containers/Playlist/Playlist.js
@@ -1,70 +1,15 @@
 import React, { useState } from 'react';
 import styles from './Playlist.module.css';
 import Track from '../../Components/Track/Track';
+import postPlaylist from '../../API Pipelines/postPlayList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons';
 export default function Playlist({ playList, removePlayListTrack, setName, name }) {
     const [nameChange, setNameChange] = useState(false);
-    
-    const retrieveID = async () => {
-        try {
-            const res = await fetch('https://api.spotify.com/v1/me?access_token=' + window.sessionStorage.getItem("token"))
-            const data = await res.json();
-            const USER_ID = data.id;
-            return USER_ID;
-        } catch (err) {
-            console.log(`ERROR: ${err}, Unable to retrieve ID`);
-        }
+
+    const handleExport = () => {
+      postPlaylist(playList, text)
     }
-
-    const POSTPLAYLIST = async () => {
-        const userID = await retrieveID();
-        const url = `https://api.spotify.com/v1/users/${userID}/playlists`;
-        try {
-            const res = await fetch(url, {
-                method: "POST",
-                body: JSON.stringify({
-                    "name": text,
-                    "description": "New playlist description",
-                    "public": false
-                }),
-                headers: {
-                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            });
-            const data = await res.json();
-            const PLAYLIST_ID = await data.id;
-            return PLAYLIST_ID;
-        } catch (err) {
-            console.log(`ERROR: ${err}, Unable to POST Playlist`);
-        }
-    }
-
-    const ADDTRACKS = async () => {
-        const playListID = await POSTPLAYLIST();
-        const url = `https://api.spotify.com/v1/playlists/${playListID}/tracks`;
-        let playListURIs = playList.map(track => track.uri);
-
-        try {
-            console.log(playListURIs)
-            const res = await fetch(url, {
-                method: "POST",
-                body: JSON.stringify({
-                    "uris": playListURIs,
-                    "position": 0,
-                }),
-                headers: {
-                    Authorization: `Bearer ${window.sessionStorage.getItem("token")}`,
-                    "Content-type": "application/json; charset=UTF-8"
-                }
-            });
-            console.log(res.json());
-        } catch (err) {
-            console.log(`ERROR: ${err}: Unable to POST tracks to playlist`);
-        }
-    }
-
 
     const [text, setText] = useState('')
     const handleClick = () => {
@@ -99,7 +44,7 @@ export default function Playlist({ playList, removePlayListTrack, setName, name 
             <div className={styles.playListTracks}>
                 {playList.map((track) => <Track track={track} key={`PlayList_${track.id}`} handleClick={removePlayListTrack} text={'x'} />)}
             </div>
-            <button className={styles.exportButton} onClick={ADDTRACKS}><FontAwesomeIcon icon={faPenToSquare} /></button>
+            <button className={styles.exportButton} onClick={handleExport}><FontAwesomeIcon icon={faPenToSquare} /></button>
 
         </div>
     )
