6997a56c2942ec56675ddb7fffd7a9faed540e49
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-04-05 22:07:24 +0000

    update handle functions with more concise language, restructure file, add comments

--- src/Containers/Playlist/Playlist.js
+++ src/Containers/Playlist/Playlist.js
@@ -3,54 +3,62 @@ import styles from './Playlist.module.css';
 import Track from '../../Components/Track/Track';
 import postPlaylist from '../../API Pipelines/postPlayList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faCheck, faCropSimple } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+
+//Component to contain playlist tracks and update playlist name
 export default function Playlist({ playList, removePlayListTrack, setName, name }) {
+
+    //Change return if user clicks edit button
     const [nameChange, setNameChange] = useState(false);
-
-    const handleExport = () => {
-      postPlaylist(playList, text)
-    }
-
-    const [text, setText] = useState('Playlist')
-    const handleClick = () => {
+    const handleEdit = () => {
         setNameChange(true)
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isBlank = () => {
-            const arr = text.split(' ');
-            console.log(arr);
-            for(let i = 0; i < text.length; i++){
-             if (arr[i] !== ''){
-                return false;
-             }else{
-                return true;
-             }
-            }
-        }
-        console.log(isBlank())
-        isBlank() || !text ? setName('Playlist') : setName(text);
-        setNameChange(false)
+    //Export playlist data to user spotify account
+    const handleExport = () => {
+        postPlaylist(playList, text)
     }
 
+
+    //Detect text change and update value
+    const [text, setText] = useState('Playlist')
     const handleChange = (e) => {
         setText(e.target.value)
     }
+
+    //Set name to text value, if none given, default to 'Playlist
+    const handleSaveName = (e) => {
+        e.preventDefault();
+        const isBlank = () => {
+            const arr = text.split(' ');
+            for (let i = 0; i < text.length; i++) {
+                if (arr[i] !== '') {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        isBlank() || !text ? setName('Playlist') : setName(text);
+        setNameChange(false);
+    }
+
+
     return (
         <div className={styles.playList}>
             <div className={styles.playListTitle}>
-                {nameChange ?
-                    <form onSubmit={handleSubmit} className={styles.playListTitle}>
+                {nameChange
+                    ?
+                    <form onSubmit={handleSaveName} className={styles.playListTitle}>
                         <input type="text" name="playlistName" placeholder={name} onChange={handleChange} />
                         <button type="submit" className={styles.editButton}><FontAwesomeIcon icon={faCheck} /></button>
                     </form>
                     :
                     <div className={styles.playListTitle}>
                         <h2>{name}</h2>
-                        <button onClick={handleClick} className={styles.editButton}><FontAwesomeIcon icon={faPenToSquare} /></button>
+                        <button onClick={handleEdit} className={styles.editButton}><FontAwesomeIcon icon={faPenToSquare} /></button>
                     </div>
-
                 }
             </div>
             <div className={styles.playListTracks}>
