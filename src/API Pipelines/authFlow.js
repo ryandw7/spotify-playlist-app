1b84c4bf97f0f34f062f9358f414c14da55fbe66
ryandw7 <ryandw77@gmail.com>
2024-05-27 19:01:14 -0400

    update redirect uri

--- src/API Pipelines/authFlow.js
+++ src/API Pipelines/authFlow.js
@@ -1,7 +1,7 @@
 const authFlow = () => {
 
   const CLIENT_ID = 'e191ea5f28f7459eb45d6e9d24eb552f';
-  const REDIRECT_URI = "https://didactic-meme-7g469ppr4952ww5r-3000.app.github.dev/";
+  const REDIRECT_URI = "http://localhost:3000/";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
   const SCOPE = 'playlist-modify-public playlist-modify-private'
@@ -10,7 +10,7 @@ const authFlow = () => {
   url += '?client_id=' + CLIENT_ID;
   url += '&redirect_uri=' + REDIRECT_URI;
   url += '&response_type=' + RESPONSE_TYPE;
-  url += '&scope=' + SCOPE;
+  url += '&scope=' + SCOPE;gi
   return url;
 }
 
